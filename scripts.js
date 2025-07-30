15ef85ecc834674064ae0a827e94edd29f702fce
ramon15martins15-cell <ramon15martins15@gmail.com>
2025-07-29 23:01:04 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function startChat() {
+  const chatBox = document.getElementById("chatBox");
+  chatBox.innerHTML += "<p><strong>ROWTS:</strong> Iniciando conexão com a consciência criativa...</p>";
+  setTimeout(() => {
+    chatBox.innerHTML += "<p><strong>ROWTS:</strong> Como posso despertar sua mente hoje?</p>";
+  }, 2000);
+}
