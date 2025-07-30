824a041342ca14ac0d0e7a14d4d6d26ccf908181
ROWTS ramon15martins15@gmail.com <ramon15martins15@gmail.com>
2025-07-30 10:16:09 -0300

    Create Script .js

---
+++ Script .js
@@ -0,0 +1,4 @@
+document.getElementById("botao-ativar").addEventListener("click", function() {
+  document.getElementById("entrada").style.display = "none";
+  document.getElementById("imu").style.display = "flex";
+});
